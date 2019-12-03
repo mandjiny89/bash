a99db957087e3c042ac50200dec8d5fcc69d9a94
Patchaiappan Mandjiny <mandjiny89@gmail.com>
2019-12-03 11:36:36 -0800

    done for and while loop

--- basic/array.sh
+++ basic/array.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # This program will hold a array and display the value it stored
 
-SERVERLIST=("server1" "server2" "server3" "server4")
+SERVERLIST=("server1" "server2" "server3" "server4" "server5") #making serverlist as a array to store the number of servers
 COUNT=0
 
-for i in ${SERVERLIST[@]}; do
+for i in ${SERVERLIST[@]}; do # @ means go through all the variable in array, we can use * also to define all
         echo "This is server count: ${SERVERLIST[COUNT]}" # Here it get the count value as 0 as first declared value
         COUNT="`expr $COUNT + 1`" # This count will trigger after the first count get the value currently the value is 0 + 1, next 1 + 1 and go on. 
 done
