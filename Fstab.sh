f302b70b2a26952f131247cdfee81e6ccfd947cb
Patchaiappan Mandjiny <mandjiny89@gmail.com>
2019-12-20 04:11:44 -0800

    added mount_check.sh

--- Fstab.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-D=`date +%Y%m%d`
-mkdir /interfaces
-mount -t nfs4 10.204.66.110:/interfaces /interfaces
-cp -prf /etc/fstab /etc/fstab_$D
-echo "10.204.66.110:/interfaces	/interfaces	nfs4    nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,_netdev,noresvport        0       0" >> /etc/fstab 
-
